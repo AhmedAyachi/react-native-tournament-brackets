b63076d8bb666709f2e2f634e449b34c525ca504
AhmedAyachi <aayachi032@gmail.com>
2022-07-17 22:10:42 +0100

    feat: Move common function to index.js

--- src/shared/components/TournamentBrackets/DoubleEliminationView/SectionView/SectionView.js
+++ src/shared/components/TournamentBrackets/DoubleEliminationView/SectionView/SectionView.js
@@ -7,22 +7,16 @@ import EliminationView from "./EliminationView/EliminationView";
 
 export default function SectionView(props){
     const {data,isElimination}=props,{title}=data;
-    console.log(data);
     return (
         <View style={css.sectionview} /* onLayout={props.onLayout} */>
             {title&&
                 <Text style={css.title} onLayout={props.onHeaderLayout}>{title}</Text>
             }
-            <EliminationView
-                {...props}
-                onLayout={props.onContentLayout}
-                style={{backgroundColor:css.sectionview.backgroundColor}}
-            />
-            {/* {(isElimination?EliminationView:ChampionshipView)({
+            {(isElimination?EliminationView:ChampionshipView)({
                 ...props,
                 onLayout:props.onContentLayout,
                 style:{backgroundColor:css.sectionview.backgroundColor},
-            })} */}
+            })}
         </View>
     )
 }
